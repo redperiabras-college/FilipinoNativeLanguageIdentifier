b5f26fa0f44853a553ac24ed85c3a8821967678b
redperiabras <redperiabras@gmail.com>
2017-04-14 19:22:09 +0800

    Updating for deployment

--- LanguageModels/LanguageModels.py
+++ LanguageModels/LanguageModels.py
@@ -2,9 +2,9 @@ import os
 from encodings.aliases import aliases
 from LanguageModels import Markov
 
-model_cebuano = Markov.MarkovChain(open(os.path.join('LanguageModels\\data\\processed', 'cebuano.txt'), encoding='utf8'),'Cebuano')
-model_kapampangan = Markov.MarkovChain(open(os.path.join('LanguageModels\\data\\processed', 'kapampangan.txt'), encoding='utf8'),'Kapampangan')
-model_pangasinan = Markov.MarkovChain(open(os.path.join('LanguageModels\\data\\processed', 'pangasinense.txt'), encoding='utf8'), 'Pangasinan')
+model_cebuano = Markov.MarkovChain(open(os.path.join('LanguageModels//data//processed', 'cebuano.txt'), encoding='utf8'),'Cebuano')
+model_kapampangan = Markov.MarkovChain(open(os.path.join('LanguageModels//data//processed', 'kapampangan.txt'), encoding='utf8'),'Kapampangan')
+model_pangasinan = Markov.MarkovChain(open(os.path.join('LanguageModels//data//processed', 'pangasinense.txt'), encoding='utf8'), 'Pangasinan')
 
 
 
